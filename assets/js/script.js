37124282f3917f7765b29d34abad48c340746a45
hgdav <aes.hur.v@gmail.com>
2024-08-14 22:36:33 -0500

    correcion

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,26 +23,23 @@ const menuItems = document.getElementById('menuItems');
 let totalGastos = 0;
 let iconosVisibles = ''; // Variable para rastrear qué íconos están visibles
 
-// Función para mostrar/ocultar íconos según el tipo seleccionado
 function alternarIconos(tipo) {
-    // Si los íconos del tipo actual ya están visibles, ocultarlos
     if (iconosVisibles === tipo) {
+        iconContainer.innerHTML = '';
         iconContainer.classList.add('hidden');
-        iconosVisibles = ''; // Reseteamos el estado a ninguno
     } else {
-        // De lo contrario, mostrar los íconos del tipo seleccionado
         iconContainer.innerHTML = ''; // Limpiar cualquier ícono previo
         const iconos = tipo === 'gastos' ? iconosGastos : iconosIngresos;
-        
-        iconos.forEach(function(icono) {
+
+        iconos.forEach(function (icono) {
             const button = document.createElement('button');
             button.className = tipo === 'gastos' ? 'icono-gasto' : 'icono-ingreso';
             button.textContent = icono.icon;
             button.setAttribute('data-monto', icono.monto);
-            button.addEventListener('click', function() {
+            button.addEventListener('click', function () {
                 const monto = parseFloat(this.getAttribute('data-monto'));
                 totalGastos += tipo === 'gastos' ? -monto : monto; // Egresos restan y ingresos suman
-                totalGastosElement.textContent = totalGastos.toFixed(2);
+                totalGastosElement.textContent = `S/${totalGastos.toFixed(2)}`;
             });
             iconContainer.appendChild(button);
         });
@@ -53,15 +50,15 @@ function alternarIconos(tipo) {
 }
 
 // Eventos para los botones de ingresos y gastos
-btnGastos.addEventListener('click', function() {
+btnGastos.addEventListener('click', function () {
     alternarIconos('gastos');
 });
 
-btnIngresos.addEventListener('click', function() {
+btnIngresos.addEventListener('click', function () {
     alternarIconos('ingresos');
 });
 
 // Funcionalidad del botón de menú desplegable
-menuButton.addEventListener('click', function() {
+menuButton.addEventListener('click', function () {
     menuItems.classList.toggle('hidden');
 });
